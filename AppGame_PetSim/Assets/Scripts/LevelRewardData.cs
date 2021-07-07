e98e2a8c9dfb89f64e853cc8ddee120e2e6ce58a
khyanghkust <khyang@connect.ust.hk>
2021-07-07 15:08:59 +0800

    Item and Save Load update

---
+++ AppGame_PetSim/Assets/Scripts/LevelRewardData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class LevelRewardData : RewardData
+{
+    public int level;
+
+    public LevelRewardData(int level, List<string> name, List<int> amount) : base(name, amount)
+    {
+        this.level = level;
+    }
+}
